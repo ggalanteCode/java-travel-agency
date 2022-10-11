a4afe303455ed2ef35e5e00656210a8e13f209e7
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-11 17:28:46 +0200

    altri metodi d'utilità, gestione eccezioni e implementazione del metodo main

--- src/com/soprasteria/javatravelagency/Agenzia.java
+++ src/com/soprasteria/javatravelagency/Agenzia.java
@@ -1,10 +1,58 @@
 package com.soprasteria.javatravelagency;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
+import com.soprasteria.javatravelagency.vacanza.Vacanza;
+
 public class Agenzia {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		String destinazione;
+		int giorno, mese, anno;
+		boolean altraDestinazione;
+		Vacanza v;
 		
+		do {
+			System.out.print("Vuoi inserire un'altra destinazione?: ");
+			altraDestinazione = s.nextBoolean();s.nextLine();
+			if(!altraDestinazione) {
+				break;
+			} else {
+				try {
+					System.out.print("Inserire la destinazione: ");
+					destinazione = s.nextLine();
+					if(destinazione.equals("")) {
+						destinazione = null;
+					}
+					v = new Vacanza(destinazione);
+					System.out.println("Data d'inizio: ");
+					System.out.print("Giorno: ");
+					giorno = s.nextInt();s.nextLine();
+					System.out.print("Mese: ");
+					mese = s.nextInt();s.nextLine();
+					System.out.print("Anno: ");
+					anno = s.nextInt();s.nextLine();
+					v.setDataInizio(LocalDate.of(anno, mese, giorno));
+					System.out.println("Data di fine: ");
+					System.out.print("Giorno: ");
+					giorno = s.nextInt();s.nextLine();
+					System.out.print("Mese: ");
+					mese = s.nextInt();s.nextLine();
+					System.out.print("Anno: ");
+					anno = s.nextInt();s.nextLine();
+					v.setDataFine(LocalDate.of(anno, mese, giorno));
+					System.out.println(v);
+				} catch(Exception e) {
+					System.err.println(e.getMessage());
+					System.err.println("Ripetere l'inserimento...\n");
+				} 
+			}
+		} while(true);
+		
+		s.close();
 	}
 
 }
--- src/com/soprasteria/javatravelagency/vacanza/Vacanza.java
+++ src/com/soprasteria/javatravelagency/vacanza/Vacanza.java
@@ -1,6 +1,8 @@
 package com.soprasteria.javatravelagency.vacanza;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class Vacanza {
 	
@@ -8,10 +10,10 @@ public class Vacanza {
 	private LocalDate dataInizio;
 	private LocalDate dataFine;
 	
-	public Vacanza(String destinazione) {
+	public Vacanza(String destinazione) throws Exception {
 		super();
-		if(destinazione.equals("")) {
-			throw new NullPointerException("la destinazione non può essere nulla!");
+		if(destinazione.equals(null)) {
+			throw new Exception("la destinazione non può essere nulla!");
 		} else {
 			this.destinazione = destinazione;
 		}
@@ -20,22 +22,16 @@ public class Vacanza {
 	public String getDestinazione() {
 		return destinazione;
 	}
-
-	public void setDestinazione(String destinazione) {
-		if(destinazione.equals("")) {
-			throw new NullPointerException("la destinazione non può essere nulla!");
-		} else {
-			this.destinazione = destinazione;
-		}
-	}
-
+	
 	public LocalDate getDataInizio() {
 		return dataInizio;
 	}
 
-	public void setDataInizio(LocalDate dataInizio) {
+	public void setDataInizio(LocalDate dataInizio) throws Exception {
 		if(dataInizio == null) {
-			throw new NullPointerException("la data di inizio non può essere nulla!");
+			throw new Exception("la data di inizio non può essere nulla!");
+		} else if(dataInizio.isBefore(LocalDate.now())) {
+			throw new Exception("la data di inizio è già passata!");
 		} else {
 			this.dataInizio = dataInizio;
 		}
@@ -45,12 +41,26 @@ public class Vacanza {
 		return dataFine;
 	}
 
-	public void setDataFine(LocalDate dataFine) {
+	public void setDataFine(LocalDate dataFine)  throws Exception {
 		if(dataFine == null) {
-			throw new NullPointerException("la data di fine non può essere nulla!");
+			throw new Exception("la data di fine non può essere nulla!");
+		} else if(dataFine.isBefore(this.dataInizio)) {
+			throw new Exception("la data di fine è prima della data d'inizio!");
 		} else {
 			this.dataFine = dataFine;
 		}
+	}
+	
+	private long durataInGiorniVacanza() {
+		return ChronoUnit.DAYS.between(this.dataInizio, this.dataFine);
+	}
+
+	@Override
+	public String toString() {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		return "Hai prenotato una vacanza di " + durataInGiorniVacanza() + 
+				" giorni a " + getDestinazione() + " dal " + 
+				getDataInizio().format(dtf) + " al " + getDataFine().format(dtf);
 	}
 
 }
